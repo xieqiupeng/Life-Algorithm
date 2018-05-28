76fae308e5f544f7f3b6f39cc5d6073ad68e7443
xieqiupeng <xieqiupeng@gmail.com>
2018-05-28 18:39:42 +0800

    update

--- sync.sh
+++ sync.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 #! 自动同步脚本
 
+git pull
+
+0_Product-Algorithm
+1_Algorithm-Me
+1.1_Aim-Chain
+1.2_Money-Flow
+1.3_Daily-Life
+2.1_Career
+2.2_My-Dear-Girl
+2.3_Relationship
+3_Product-Me
+Concealment
+Do-Things
+Knowledge-Book
+Technology-and-Expertise
