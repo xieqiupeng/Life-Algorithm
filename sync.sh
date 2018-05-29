99368694ed4735115de6ebc4f3f738c6227fc14f
xieqiupeng <xieqiupeng@gmail.com>
2018-05-29 15:45:17 +0800

    update

--- sync.sh
+++ sync.sh
@@ -4,15 +4,20 @@
 git pull
 
 0_Product-Algorithm
+
 1_Algorithm-Me
 1.1_Aim-Chain
 1.2_Money-Flow
 1.3_Daily-Life
+
+2_Customer-Profile
 2.1_Career
 2.2_My-Dear-Girl
 2.3_Relationship
+
 3_Product-Me
 3.0.0_Workout
+
 Concealment
 Do-Things
 Knowledge-Book
