faacc999d0e48d08da726a2bf590f0f9429d72fc
xieqiupeng <xieqiupeng@gmail.com>
2018-06-01 10:43:00 +0800

    update

--- sync.sh
+++ sync.sh
@@ -31,11 +31,20 @@
 
 # pull all
 export ROOT=/Users/Cobb/Documents/Life-Algorithm
+cd $ROOT
 git pull origin
 cd $ROOT/0_Product-Algorithm
 git pull origin
+
 cd $ROOT/1_Algorithm-Me
 git pull origin
+cd $ROOT/1_Algorithm-Me/1.1_Aim-Chain
+git pull origin
+cd $ROOT/1_Algorithm-Me/1.2_Money-Flow
+git pull origin
+cd $ROOT/1_Algorithm-Me/1.3_Daily-Life
+git pull origin
+
 cd $ROOT/2_Customer-Profile
 git pull origin
 cd $ROOT/2_Customer-Profile/2.1_Career
@@ -44,11 +53,13 @@ cd $ROOT/2_Customer-Profile/2.2_My-Dear-Girl
 git pull origin
 cd $ROOT/2_Customer-Profile/2.3_Relationship
 git pull origin
+
 cd $ROOT/3_Product-Me
 git pull origin
 cd $ROOT/3_Product-Me/3.1_Workout
 git pull origin
 cd $ROOT/3_Product-Me/3.2_Conversation
 git pull origin
+
 cd $ROOT/4_Scene
 git pull origin
