3ae8564d0d3aee791c211f061e33e1da85376e81
xieqiupeng <xieqiupeng@gmail.com>
2018-06-01 10:46:56 +0800

    update

--- sync.sh
+++ sync.sh
@@ -1,30 +1,6 @@
 #!/usr/bin/env bash
 # 自动同步脚本
 
-### 全部仓库
-# 0_Product-Algorithm
-#
-# 1_Algorithm-Me
-# 1.1_Aim-Chain
-# 1.2_Money-Flow
-# 1.3_Daily-Life
-#
-# 2_Customer-Profile
-# 2.1_Career
-# 2.2_My-Dear-Girl
-# 2.3_Relationship
-#
-# 3_Product-Me
-# 3.1_Workout
-# 3.2_Conversation
-#
-# 4_Scene
-#
-# Concealment
-# Do-Things
-# Knowledge-Book
-# Technology-and-Expertise
-
 # fetch all
 # git remote show origin
 # git fetch https://github.com/xieqiupeng/Life-Algorithm master
@@ -63,7 +39,11 @@ git pull origin
 
 cd $ROOT/4_Scene
 git pull origin
+cd $ROOT/Concealment
+git pull origin
 cd $ROOT/Do-Things
 git pull origin
 cd $ROOT/Technology-and-Expertise
+git pull origin
+cd $ROOT/Knowledge-Book
 git pull origin
