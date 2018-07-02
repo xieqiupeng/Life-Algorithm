15c4840a50cb3a67c2f4e815beebbf40327bacc2
xieqiupeng <xieqiupeng@gmail.com>
2018-07-02 15:00:42 +0800

    updatee

---
+++ FastFolder.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == '__main__':
+    abs = os.path.abspath(__file__)
+    real = os.path.realpath(__file__)
+    dirname = os.path.dirname(real)
+    cwd = os.getcwd()
+    list = os.listdir(dirname)
+    print "\n\t" + abs + "\n\t" + real + "\n\t" + dirname + "\n\t" + cwd + "\n\t" + list
--- sort.sh
+++ sort.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # 编号脚本
+# ln -s ./4/Customer-Profile/3.4/Ecology/3.4.1/iGetGet/ ./iGetGet
 
 export ROOT=/Users/Cobb/Documents/Life-Algorithm
 cd $ROOT
 cd /Users/Cobb/Documents/Life-Algorithm
-ln -s ./4/Customer-Profile/3.4/Ecology/3.4.1/iGetGet/ ./iGetGet
+
 
 cd $ROOT
