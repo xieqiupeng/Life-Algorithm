b7bf8c25c15c26d9d32e3529cec908b07fbc0606
xieqiupeng <xieqiupeng@gmail.com>
2018-07-02 17:40:02 +0800

    update

--- FastFolder.py
+++ FastFolder.py
@@ -4,28 +4,28 @@ import os
 import sys
 import re
 
-def cdNextFolder( root ):
-    list = os.listdir(root)
+# 遍历所有序号仓库
+def cdNextFolder( all ):
     pattern = re.compile(r'^[0-9]$|([0-9]\.){1,}[0-9]')
-    for folder in list:
-        if not os.path.isdir(folder):
-            continue
-            print folder
-        match = pattern.match(folder)
-        # 找到序号文件夹
-        if match:
-            target = match.group(0)
-            os.chdir(target)
-            child = os.getcwd()
-            # print(child)
-            childList = os.listdir(child)
-            # print(childList)
-            for each in childList:
-                print(each)
-                # cdNextFolder( child )
-            os.chdir('..')
-        else:
-            continue
+    for root, dirs, files in os.walk(all):
+        for each in dirs:
+            match = pattern.match(each)
+            if match:
+                target = match.group(0)
+                print(target)
+                # 序号文件夹
+                os.chdir(target)
+                child = os.getcwd()
+                listNextFolder(child)
+                os.chdir('..')
+
+# 打印序号名称和仓库名称
+def listNextFolder( all ):
+    print(all)
+    dirs = os.listdir(all)
+    for each in dirs:
+        if os.path.isdir(each):
+            print(each)
 
 if __name__ == '__main__':
     abs = os.path.abspath(__file__)
