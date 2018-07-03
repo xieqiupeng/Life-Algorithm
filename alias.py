8a9cd54a79ada57bc8ed27998d7f87ab57cb60ad
xieqiupeng <xieqiupeng@gmail.com>
2018-07-03 19:09:46 +0800

    update

--- alias.py
+++ alias.py
@@ -7,29 +7,18 @@ import re
 # 单个数字 or 多级数字必须以数字结尾
 pattern = re.compile(r'^[0-9]$|^([0-9]\.){1,}[0-9]$')
 
-# 打印文件
-def getList():
-    cwd = os.getcwd();
-    for root, dirs, files in os.walk(cwd):
-        for each in dirs:
-            match = pattern.match(each)
-            if match:
-                target = match.group(0)
-                # 打印仓库序号
-                print root + "/" + target
+# 打印仓库列表
+def showList( root, target ):
+    print root + "/" + target
+    os.chdir(root)
+    os.chdir(target)
+    dirs = os.listdir(os.getcwd())
+    for each in dirs:
+        if os.path.isdir(each):
+            print target + "_" + each
 
-# 遍历所有序号仓库
-def getAlias():
-    cwd = os.getcwd();
-    for root, dirs, files in os.walk(cwd):
-        for each in dirs:
-            match = pattern.match(each)
-            if match:
-                target = match.group(0)
-                showChild(root, target)
-
-# 打印仓库正式名称
-def showChild( root, target ):
+# 打印alias命令
+def showAlias( root, target ):
     os.chdir(root)
     os.chdir(target)
     dirs = os.listdir(os.getcwd())
@@ -37,15 +26,20 @@ def showChild( root, target ):
         if os.path.isdir(each):
             shell = "ln -s " + root + "/" + target + "/" + each + " " + target + "_" + each
             print shell
-            # print target + "_" + each
 
 #
 def __main__():
     if len(sys.argv) == 1:
-        return
-    if sys.argv[1] == "getAlias":
-        getAlias()
-    else:
-        getList()
+        sys.argv.append("getList")
+    cwd = os.getcwd();
+    for root, dirs, files in os.walk(cwd):
+        for each in dirs:
+            match = pattern.match(each)
+            if match:
+                target = match.group(0)
+                if sys.argv[1] == "getAlias":
+                    showAlias(root, target)
+                else:
+                    showList(root, target)
 
 __main__()
