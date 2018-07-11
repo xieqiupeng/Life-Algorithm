a1c6143746627e537a8848bab91ff7606f9358cc
xieqiupeng <xieqiupeng@gmail.com>
2018-07-11 18:38:56 +0800

    update

--- alias.py
+++ alias.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 import os
 import sys
 import re
 
 # 单个数字 or 多级数字必须以数字结尾
 pattern = '^[0-9]$|^([0-9]\.){1,}[0-9]$'
-path = "";
-name = "";
+path = ""
+name = ""
+
 
 # 打印仓库列表
-def getList( root, target ):
+def get_list(root, target):
     print root + "/" + target
     os.chdir(root)
     os.chdir(target)
@@ -19,8 +20,9 @@ def getList( root, target ):
         if os.path.isdir(each):
             print target + "_" + each
 
+
 # 打印alias命令
-def getAlias( root, target ):
+def get_alias(root, target):
     global path
     global name
     os.chdir(root)
@@ -28,10 +30,11 @@ def getAlias( root, target ):
     path = "ln -s " + root + "/" + target + "/"
     name = target + "_"
     dirs = os.listdir(os.getcwd())
-    dfs( dirs )
+    dfs(dirs)
+
 
 # Depth-First-Search
-def dfs( dirs ):
+def dfs(dirs):
     global path
     global name
     for index in range(len(dirs)):
@@ -50,15 +53,17 @@ def dfs( dirs ):
             # path = path + each
             # name = name + each
 
+
 # Breadth-First-Search
-def bfs( dirs ):
+def bfs(dirs):
     for each in dirs:
         if os.path.isdir(each):
             shell = "ln -s " + root + "/" + target + "/" + each + " " + target + "_" + each
             print shell
 
+
 # pattern
-def getPattern():
+def get_pattern():
     global pattern
     if len(sys.argv) == 1:
         # 指定长度
@@ -71,25 +76,27 @@ def getPattern():
     regex = re.compile(r'' + pattern)
     return regex
 
+
 #
 def __main__():
-    regex = getPattern()
+    regex = get_pattern()
     if len(sys.argv) == 3:
         sys.argv.append("getAlias")
-    cwd = os.getcwd();
+    cwd = os.getcwd()
     for root, dirs, files in os.walk(cwd):
         for each in dirs:
             match = regex.match(each)
             if match:
                 target = match.group(0)
                 if sys.argv[3] == "getAlias":
-                    getAlias(root, target)
+                    get_alias(root, target)
                     continue
                 if sys.argv[3] == "getList":
-                    getList(root, target)
+                    get_list(root, target)
                     continue
                 else:
-                    getAlias(root, target)
+                    get_alias(root, target)
                     continue
+
 
 __main__()
