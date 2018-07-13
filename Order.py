a5591cf2d0c4570efd2839141ce5394afc8a3c85
xieqiupeng <xieqiupeng@gmail.com>
2018-07-13 14:10:24 +0800

    update

--- Order.py
+++ Order.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
 import os
-import re
-import sys
+from Patter import *
 
 path = ""
 sequence = ""
 name = ""
-param = []
 
 
 # 打印序号
 def get_sequence(root):
     global sequence
-    regex = get_pattern()
     levels = root.split("/")
     for index in range(len(levels)):
         each = levels[index]
+        regex = get_pattern()
+        # regex = folder_match(index)
         match = regex.match(each)
         if match:
             relative = match.group(0)
@@ -60,36 +59,6 @@ def dfs(dirs):
             continue
         else:
             continue
-
-
-# pattern
-def get_pattern():
-    global param
-    if len(sys.argv) == 1:
-        # 指定前缀
-        # 单个数字
-        pattern = "^[0-9]$"
-        regex = re.compile(r'' + pattern)
-        return regex
-    if len(sys.argv) == 2:
-        # 单个数字
-        pattern1 = "^[0-9]$"
-        regex1 = re.compile(r'' + pattern1)
-        match1 = regex1.match(sys.argv[1])
-        #
-        # 多级数字必须以数字结尾
-        pattern2 = "^([0-9]\.){1,}[0-9]$"
-        regex2 = re.compile(r'' + pattern2)
-        match2 = regex2.match(sys.argv[1])
-        if match1:
-            pattern1 = "^" + sys.argv[1] + "$"
-            regex1 = re.compile(r'' + pattern1)
-            return regex1
-        if match2:
-            param = sys.argv[1].split(".")
-            pattern2 = "^" + param[0] + "$"
-            regex2 = re.compile(r'' + pattern2)
-            return regex2
 
 
 #
---
+++ Patter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding=utf-8
+import re
+import sys
+
+param = []
+
+
+# pattern
+def get_pattern():
+    global param
+    if len(sys.argv) == 1:
+        # 指定前缀
+        # 单个数字
+        pattern = "^[0-9]$"
+        regex = re.compile(r'' + pattern)
+        return regex
+    if len(sys.argv) == 2:
+        # 单个数字
+        pattern1 = "^[0-9]$"
+        regex1 = re.compile(r'' + pattern1)
+        match1 = regex1.match(sys.argv[1])
+        #
+        # 多级数字必须以数字结尾
+        pattern2 = "^([0-9]\.){1,}[0-9]$"
+        regex2 = re.compile(r'' + pattern2)
+        match2 = regex2.match(sys.argv[1])
+        if match1:
+            pattern1 = "^" + sys.argv[1] + "$"
+            regex1 = re.compile(r'' + pattern1)
+            return regex1
+        if match2:
+            param = sys.argv[1].split(".")
+            print(param)
+            pattern2 = "^" + param[0] + "$"
+            regex2 = re.compile(r'' + pattern2)
+            return regex2
+
+
+# 每级
+def folder_match(index):
+    pattern = "^" + param[index] + "$"
+    regex = re.compile(r'' + pattern)
+    return regex
