ac1f1ad9c9a98a9a25f5b5c05573efd291ff2862
xieqiupeng <xieqiupeng@gmail.com>
2018-07-13 11:26:21 +0800

    update

--- Order.py
+++ Order.py
@@ -2,12 +2,12 @@
 # coding=utf-8
 import os
 import re
+import sys
 
-# 单个数字 or 多级数字必须以数字结尾
-pattern = '^[0-9]$'
 path = ""
-name = ""
 sequence = ""
+name = ""
+param = []
 
 
 # 打印序号
@@ -43,8 +43,8 @@ def get_alias(root, target):
 # Depth-First-Search
 def dfs(dirs):
     global path
-    global name
     global sequence
+    global name
     # print(dirs)
     # filter()
     for index in range(len(dirs)):
@@ -64,15 +64,40 @@ def dfs(dirs):
 
 # pattern
 def get_pattern():
-    global pattern
-    regex = re.compile(r'' + pattern)
-    return regex
+    global param
+    if len(sys.argv) == 1:
+        # 指定前缀
+        # 单个数字
+        pattern = "^[0-9]$"
+        regex = re.compile(r'' + pattern)
+        return regex
+    if len(sys.argv) == 2:
+        # 单个数字
+        pattern1 = "^[0-9]$"
+        regex1 = re.compile(r'' + pattern1)
+        match1 = regex1.match(sys.argv[1])
+        #
+        # 多级数字必须以数字结尾
+        pattern2 = "^([0-9]\.){1,}[0-9]$"
+        regex2 = re.compile(r'' + pattern2)
+        match2 = regex2.match(sys.argv[1])
+        if match1:
+            pattern1 = "^" + sys.argv[1] + "$"
+            regex1 = re.compile(r'' + pattern1)
+            return regex1
+        if match2:
+            param = sys.argv[1].split(".")
+            pattern2 = "^" + param[0] + "$"
+            regex2 = re.compile(r'' + pattern2)
+            return regex2
 
 
 #
 def __main__():
     regex = get_pattern()
     cwd = os.getcwd()
+    # dirs 文件夹
+    # files 文件
     for root, dirs, files in os.walk(cwd):
         for each in dirs:
             # 匹配每个文件夹
