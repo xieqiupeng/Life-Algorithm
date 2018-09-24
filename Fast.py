0631aae640be0ae47c1e6da263b2f70af63be35d
xieqiupeng <xieqiupeng@gmail.com>
2018-09-24 18:31:03 +0800

    update

---
+++ Fast.py
@@ -0,0 +1,14 @@
+ln -s /Volumes/SSD/Mood/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood
+
+ln -s /Volumes/SSD/Point-High/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/1/Point-High
+ln -s /Volumes/SSD/Point-Pain/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/2/Point-Pain
+ln -s /Volumes/SSD/Point-Itchy/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/3/Point-Itchy
+
+ln -s /Volumes/SSD/Strategy/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy
+
+ln -s /Volumes/SSD/Value-Final/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/1/Value-Final
+ln -s /Volumes/SSD/Value-Peak/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/2/Value-Peak
+ln -s /Volumes/SSD/Bottom-Line/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/3/Bottom-Line
+
+ln -s /Volumes/SSD/Aim-Chain/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/其他/Aim-Chain
+
