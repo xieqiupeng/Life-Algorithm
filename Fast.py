9a1300500b8b90e77a5d2ca863c35039ad1d4044
xieqiupeng <xieqiupeng@gmail.com>
2018-09-24 19:29:22 +0800

    update

--- Fast.py
+++ Fast.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Algorithm-Me
+ln -s /Volumes/SSD/Algorithm-Me/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me
+
+# Mood
 ln -s /Volumes/SSD/Mood/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood
 
 ln -s /Volumes/SSD/Point-High/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/1/Point-High
 ln -s /Volumes/SSD/Point-Pain/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/2/Point-Pain
 ln -s /Volumes/SSD/Point-Itchy/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/3/Point-Itchy
 
+# Strategy
 ln -s /Volumes/SSD/Strategy/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy
 
 ln -s /Volumes/SSD/Value-Final/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/1/Value-Final
@@ -11,4 +19,26 @@ ln -s /Volumes/SSD/Value-Peak/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Stra
 ln -s /Volumes/SSD/Bottom-Line/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/3/Bottom-Line
 
 ln -s /Volumes/SSD/Aim-Chain/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/其他/Aim-Chain
+
+# Career
+ln -s /Volumes/SSD/Career/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career
+
+ln -s /Volumes/SSD/Expertise/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/1/Expertise
+ln -s /Volumes/SSD/Technology-and-Expertise/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/2/Technology-and-Expertise
+ln -s /Volumes/SSD/Coitus-Power/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/3/Coitus-Power
+
+ln -s /Volumes/SSD/Workout/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/3/Coitus-Power/Workout
+
+# Resource
+ln -s  /Volumes/SSD/Level-Resource /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource
+
+ln -s /Volumes/SSD/Resource-Investment/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/1/Resource-Investment
+ln -s /Volumes/SSD/Resource-Consumption/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/2/Resource-Consumption
+ln -s /Volumes/SSD/Resource-Export/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/3/Resource-Export
+ln -s /Volumes/SSD/Resource-Estate/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/4/Resource-Estate
+ln -s /Volumes/SSD/Resource-Rare/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/5/Resource-Rare
+
+ln -s /Volumes/SSD/Tools-and-Weapons/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/5/Tools-and-Weapons
+
+
 
