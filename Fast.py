a3c46bd78d7a7bcf636caaa0263f502250923b6c
xieqiupeng <xieqiupeng@gmail.com>
2018-09-28 17:05:03 +0800

    update

--- Fast.py
+++ Fast.py
@@ -21,6 +21,9 @@ ln -s /Volumes/SSD/Bottom-Line/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Str
 ln -s /Volumes/SSD/Economics/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/3/Bottom-Line/3/Economics
 ln -s /Volumes/SSD/Aim-Chain/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/其他/Aim-Chain
 
+# Ecology
+ln -s /Volumes/SSD/Ecology-Niche/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Ecology-Niche
+
 # Career
 ln -s /Volumes/SSD/Career/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career
 
@@ -31,15 +34,20 @@ ln -s /Volumes/SSD/Coitus-Power/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Ca
 ln -s /Volumes/SSD/Workout/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/3/Coitus-Power/Workout
 
 # Resource
-ln -s  /Volumes/SSD/Level-Resource /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource
+ln -s  /Volumes/SSD/Level-Resource /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource
 
-ln -s /Volumes/SSD/Resource-Investment/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/1/Resource-Investment
-ln -s /Volumes/SSD/Resource-Consumption/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/2/Resource-Consumption
-ln -s /Volumes/SSD/Resource-Export/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/3/Resource-Export
-ln -s /Volumes/SSD/Resource-Estate/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/4/Resource-Estate
-ln -s /Volumes/SSD/Resource-Rare/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/5/Resource-Rare
+ln -s /Volumes/SSD/Attention/                /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/1/Attention
+ln -s /Volumes/SSD/Time-Currency/            /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/2/Time-Currency
+ln -s /Volumes/SSD/Resource-Estate/          /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/3/Resource-Estate
+ln -s /Volumes/SSD/Perfect-Body/             /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/4/Perfect-Body
+ln -s /Volumes/SSD/Gnix-Pet/                 /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/5/Gnix-Pet
+ln -s /Volumes/SSD/Money-Flow/               /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/6/Money-Flow
+ln -s /Volumes/SSD/Tools-and-Weapons/        /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/7/Tools-and-Weapons
+ln -s /Volumes/SSD/Resource-Rare/            /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/8/Resource-Rare
 
-ln -s /Volumes/SSD/Tools-and-Weapons/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/4/Level-Resource/5/Tools-and-Weapons
+ln -s /Volumes/SSD/Investment/               /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/1/Investment
+ln -s /Volumes/SSD/Consumption/              /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/2/Consumption
+ln -s /Volumes/SSD/Export/                   /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource/3/Export
 
 # Product-Me
 ln -s /Volumes/SSD/Product-Me/ /Volumes/SSD/Life-Algorithm/3/Product-Me
@@ -51,11 +59,19 @@ ln -s /Volumes/SSD/Intimate/ /Volumes/SSD/Life-Algorithm/3/Product-Me/3/Intimate
 # Customer-Profile
 ln -s /Volumes/SSD/Customer-Profile/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile
 
-ln -s /Volumes/SSD/Level-Accost/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Level-Accost
-ln -s /Volumes/SSD/Level-Date/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/2/Level-Date
-ln -s /Volumes/SSD/Level-Domesticate/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/3/Level-Domesticate
+ln -s /Volumes/SSD/Relationship /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Relationship
+
+ln -s /Volumes/SSD/Level-Accost/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Relationship/1/Level-Accost
+ln -s /Volumes/SSD/Level-Date/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Relationship/2/Level-Date
+ln -s /Volumes/SSD/Level-Domesticate/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Relationship/3/Level-Domesticate
+
 ln -s /Volumes/SSD/Tutorial/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/4/Tutorial
-ln -s /Volumes/SSD/Relationship /Volumes/SSD/Life-Algorithm/4/Customer-Profile/5/Relationship
+ln -s /Volumes/SSD/Link /Volumes/SSD/Life-Algorithm/4/Customer-Profile/5/Link
+
+# Market
+ln -s /Volumes/SSD/Market /Volumes/SSD/Life-Algorithm/4/Customer-Profile/6/Market
+ln -s /Volumes/SSD/Market-Welcome /Volumes/SSD/Life-Algorithm/4/Customer-Profile/6/Market/3/Market-Welcome
+ln -s /Volumes/SSD/Society/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/6/Market/3/Market-Welcome/1/Society
 
 # Scene
 ln -s /Volumes/SSD/Scene/ /Volumes/SSD/Life-Algorithm/5/Scene
