aa47beb750dc5e5cdd873573720a2478c3973d54
xieqiupeng <xieqiupeng@gmail.com>
2018-10-16 00:14:43 +0800

    update

--- Fast.py
+++ Fast.py
@@ -17,13 +17,6 @@ ln -s /Volumes/SSD/Point-Itchy/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Moo
 # Strategy
 ln -s /Volumes/SSD/Strategy/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy
 
-ln -s /Volumes/SSD/Value-Final/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/1/Value-Final
-ln -s /Volumes/SSD/Value-Peak/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/2/Value-Peak
-ln -s /Volumes/SSD/Bottom-Line/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/3/Bottom-Line
-
-ln -s /Volumes/SSD/Economics/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/3/Bottom-Line/3/Economics
-ln -s /Volumes/SSD/Aim-Chain/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy/其他/Aim-Chain
-
 # Ecology
 ln -s /Volumes/SSD/Ecology-Niche/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Ecology-Niche
 
@@ -44,20 +37,6 @@ ln -s /Volumes/SSD/Product-Me/ /Volumes/SSD/Life-Algorithm/3/Product-Me
 # Customer-Profile
 ln -s /Volumes/SSD/Customer-Profile/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile
 
-ln -s /Volumes/SSD/Relationship /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Relationship
-
-ln -s /Volumes/SSD/Level-Accost/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Relationship/1/Level-Accost
-ln -s /Volumes/SSD/Level-Date/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Relationship/2/Level-Date
-ln -s /Volumes/SSD/Level-Domesticate/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/1/Relationship/3/Level-Domesticate
-
-ln -s /Volumes/SSD/Tutorial/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/4/Tutorial
-ln -s /Volumes/SSD/Link /Volumes/SSD/Life-Algorithm/4/Customer-Profile/5/Link
-
-# Market
-ln -s /Volumes/SSD/Market /Volumes/SSD/Life-Algorithm/4/Customer-Profile/6/Market
-ln -s /Volumes/SSD/Market-Welcome /Volumes/SSD/Life-Algorithm/4/Customer-Profile/6/Market/3/Market-Welcome
-ln -s /Volumes/SSD/Society/ /Volumes/SSD/Life-Algorithm/4/Customer-Profile/6/Market/3/Market-Welcome/1/Society
-
 # Scene
 ln -s /Volumes/SSD/Scene/ /Volumes/SSD/Life-Algorithm/5/Scene
 
@@ -70,8 +49,6 @@ ln -s /Volumes/SSD/Data-Me/ /Volumes/SSD/Life-Algorithm/1/Data-Me
 ln -s /Volumes/SSD/Cognition/ /Volumes/SSD/Life-Algorithm/1/Data-Me/2/数据清洗/Cognition
 ln -s /Volumes/SSD/Credential/ /Volumes/SSD/Life-Algorithm/1/Data-Me/3/数据存储/Credential
 ln -s /Volumes/SSD/iGetGet/ /Volumes/SSD/Life-Algorithm/1/Data-Me/4/数据分析/Division/1/iGetGet
-
-ln -s /Volumes/SSD/Economics/ /Volumes/SSD/Life-Algorithm/1/Data-Me/4/数据分析/Division/1/iGetGet/5/Economics
 
 # wiki
 ln -s /Volumes/SSD/wiki/ /Volumes/SSD/Life-Algorithm/wiki
