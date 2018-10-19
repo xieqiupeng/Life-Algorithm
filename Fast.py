8b8e59f92bace4de7bcf469232a88008d76e99ca
xieqiupeng <xieqiupeng@gmail.com>
2018-10-19 14:16:30 +0800

    update

--- Fast.py
+++ Fast.py
@@ -7,31 +7,6 @@ ln -s /Volumes/SSD/Operating-System/ ./0/Operating-System
 # Algorithm-Me
 ln -s /Volumes/SSD/Algorithm-Me/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me
 
-# Mood
-ln -s /Volumes/SSD/Mood/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood
-
-ln -s /Volumes/SSD/Point-High/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/1/Point-High
-ln -s /Volumes/SSD/Point-Pain/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/2/Point-Pain
-ln -s /Volumes/SSD/Point-Itchy/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/1/Mood/3/Point-Itchy
-
-# Strategy
-ln -s /Volumes/SSD/Strategy/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/2/Strategy
-
-# Ecology
-ln -s /Volumes/SSD/Ecology-Niche/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Ecology-Niche
-
-# Career
-ln -s /Volumes/SSD/Career/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career
-
-ln -s /Volumes/SSD/Expertise/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/1/Expertise
-ln -s /Volumes/SSD/Technology-and-Expertise/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/2/Technology-and-Expertise
-ln -s /Volumes/SSD/Coitus-Power/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/3/Coitus-Power
-
-ln -s /Volumes/SSD/Workout/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/3/Career/3/Coitus-Power/Workout
-
-# Resource
-ln -s  /Volumes/SSD/Level-Resource /Volumes/SSD/Life-Algorithm/2/Algorithm-Me/5/Level-Resource
-
 ln -s /Volumes/SSD/Product-Me/ /Volumes/SSD/Life-Algorithm/3/Product-Me
 
 # Customer-Profile
