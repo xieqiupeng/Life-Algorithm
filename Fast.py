2b53b72cc0ea8bb83e651d94a99af6912162a58b
xieqiupeng <xieqiupeng@gmail.com>
2018-10-23 16:35:08 +0800

    udptea

--- Fast.py
+++ Fast.py
@@ -4,9 +4,13 @@
 #
 ln -s /Volumes/SSD/Operating-System/ ./0/Operating-System
 
+# Data-Me
+ln -s /Volumes/SSD/Data-Me/ /Volumes/SSD/Life-Algorithm/1/Data-Me
+
 # Algorithm-Me
 ln -s /Volumes/SSD/Algorithm-Me/ /Volumes/SSD/Life-Algorithm/2/Algorithm-Me
 
+# Product-Me
 ln -s /Volumes/SSD/Product-Me/ /Volumes/SSD/Life-Algorithm/3/Product-Me
 
 # Customer-Profile
@@ -17,13 +21,6 @@ ln -s /Volumes/SSD/Scene/ /Volumes/SSD/Life-Algorithm/5/Scene
 
 # Story
 ln -s /Volumes/SSD/Story/ /Volumes/SSD/Life-Algorithm/6/Story
-
-# Data-Me
-ln -s /Volumes/SSD/Data-Me/ /Volumes/SSD/Life-Algorithm/1/Data-Me
-
-ln -s /Volumes/SSD/Cognition/ /Volumes/SSD/Life-Algorithm/1/Data-Me/2/数据清洗/Cognition
-ln -s /Volumes/SSD/Credential/ /Volumes/SSD/Life-Algorithm/1/Data-Me/3/数据存储/Credential
-ln -s /Volumes/SSD/iGetGet/ /Volumes/SSD/Life-Algorithm/1/Data-Me/4/数据分析/Division/1/iGetGet
 
 # wiki
 ln -s /Volumes/SSD/wiki/ /Volumes/SSD/Life-Algorithm/wiki
