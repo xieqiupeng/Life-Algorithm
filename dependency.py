ebe51999d84d1eb43e3bb10089db6fc2e1c043ee
xieqiupeng <xieqiupeng@gmail.com>
2018-11-30 10:59:19 +0800

    udpate

--- dependency.py
+++ dependency.py
@@ -5,11 +5,11 @@
 cd /Volumes/Rep/Life-Algorithm
 
 # Life-Algorithm
-ln -s /Volumes/Rep/Operating-System  ./0/Operating-System
-ln -s /Volumes/Rep/Data-Me  ./1/Data-Me
-ln -s /Volumes/Rep/Algorithm-Me  ./2/Algorithm-Me
-ln -s /Volumes/Rep/Product-Me  ./3/Product-Me
-ln -s /Volumes/Rep/Customer-Profile  ./4/Customer-Profile
-ln -s /Volumes/Rep/Scene  ./5/Scene
-ln -s /Volumes/Rep/Story  ./6/Story
-ln -s /Volumes/Rep/wiki  ./wiki
+ln -sf /Volumes/Rep/Operating-System  ./0/Operating-System
+ln -sf /Volumes/Rep/Data-Me  ./1/Data-Me
+ln -sf /Volumes/Rep/Algorithm-Me  ./2/Algorithm-Me
+ln -sf /Volumes/Rep/Product-Me  ./3/Product-Me
+ln -sf /Volumes/Rep/Customer-Profile  ./4/Customer-Profile
+ln -sf /Volumes/Rep/Scene  ./5/Scene
+ln -sf /Volumes/Rep/Story  ./6/Story
+ln -sf /Volumes/Rep/wiki  ./wiki
