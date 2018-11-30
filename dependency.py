d2fbb6164b65199d3a99550ee6fa33a5ad7b6f9d
xieqiupeng <xieqiupeng@gmail.com>
2018-11-30 10:58:20 +0800

    udpate

---
+++ dependency.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Life-Algorithm
+cd /Volumes/Rep/Life-Algorithm
+
+# Life-Algorithm
+ln -s /Volumes/Rep/Operating-System  ./0/Operating-System
+ln -s /Volumes/Rep/Data-Me  ./1/Data-Me
+ln -s /Volumes/Rep/Algorithm-Me  ./2/Algorithm-Me
+ln -s /Volumes/Rep/Product-Me  ./3/Product-Me
+ln -s /Volumes/Rep/Customer-Profile  ./4/Customer-Profile
+ln -s /Volumes/Rep/Scene  ./5/Scene
+ln -s /Volumes/Rep/Story  ./6/Story
+ln -s /Volumes/Rep/wiki  ./wiki
